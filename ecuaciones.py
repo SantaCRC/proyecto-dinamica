38423428516024376fa9a8b1c62f76fd6e08cff1
Fabian <alvarez.fabian@outlook.com>
2021-04-26 21:54:13 -0600

    Add

---
+++ ecuaciones.py
@@ -0,0 +1,46 @@
+from sympy.solvers import solve
+from sympy import Symbol, symbols,Eq
+ma,va,mb,vb,vap,vbp,e,h,u,x=symbols('ma va mb vb vap vbp e h u x')
+
+#ecuaciones momento
+eqn1=Eq(ma*va+mb*vb,ma*vap+mb*vbp)
+eqn2=Eq((va-vb)*e,vbp-vap)
+#ecuaciones de conservacion de energia
+eqn3=Eq(1/2*ma*vap**2,ma*9.81*h)
+eqn4=1/2*mb*vbp**2-u*mb*9.81*x
+
+
+#Variables
+e_v=0.8
+ma_v=0.5
+va_v=0
+mb_v=1
+vb_v=2
+u_v=0.6
+
+#Funcion helper de asignacion de Variables
+def set_vars(e_va,ma_va,va_va,mb_va,vb_va,u_va):
+    global e_v,ma_v,va_v,mb_v,vb_v,u_v
+    e_v=e_va
+    ma_v=ma_va
+    va_v=va_va
+    mb_v=mb_va
+    vb_v=vb_va
+    u_v=u_va
+
+def resolver():
+    res=solve((eqn1,eqn2),vbp,vap)
+    res1=solve(eqn3,h)
+    res2=solve(eqn4,x)
+    v_a_p=res[vap]
+    v_b_p=res[vbp]
+    v_a_p=v_a_p.subs([(e,e_v),(ma,ma_v),(va,va_v),(mb,mb_v),(vb,vb_v)])
+    v_b_p=v_b_p.subs([(e,e_v),(ma,ma_v),(va,va_v),(mb,mb_v),(vb,vb_v)])
+    h_v=res1[0].subs([(ma,ma_v),(vap,v_a_p)])
+    x_v=res2[0].subs([(mb,mb_v),(u,u_v),(vbp,v_b_p)])
+    print(h_v)
+    return h_v,x_v
+
+if __name__ == '__main__':
+    print(h_v)
+    print(x_v)
---
+++ test1.py
@@ -0,0 +1,65 @@
+import pygame, sys, math
+import ecuaciones
+from tkinter import *
+import threading
+
+from tkinter import messagebox
+
+
+def animar():
+    h_v,x_v=ecuaciones.resolver()
+    run = True
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    angle = 1.57
+    x,y=10,10
+    size = width, height = 600, 600
+    screen = pygame.display.set_mode(size)
+    xb,yb=400,350
+
+    clock = pygame.time.Clock()
+    screen.fill(white)
+    bloque = pygame.image.load("bloque.png")
+
+    while run:
+        msElapsed = clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        screen.fill(white)
+        screen.blit(bloque,(xb,yb))
+        pygame.draw.line(screen,black,(300,200),(x,y))
+        pygame.draw.rect(screen, black, pygame.Rect(xb, yb, 90, 70))
+        pygame.draw.circle(screen,black,(x,y),20)
+        y=int(math.sin(angle) * 200+200)
+        x=int(math.cos(angle) * 200+300)
+        if(xb>100):
+            xb-=3
+        grados=(angle*180)/math.pi-90
+        h=h_v
+        xa=x_v
+        max=70
+        if(xb<300 and grados<=max):
+            angle+=0.05
+
+        pygame.display.flip()
+
+    pygame.quit()
+
+top = Tk()
+top.geometry("300x300")
+def helloCallBack():
+   ecuaciones.set_vars(float(txt.get()),float(txt1.get()),0,1,2,0.6)
+   animar()
+
+B = Button(top, text = "Simular", command = helloCallBack)
+label= Label(top, text="Coeficiente de friccion dinamico:")
+label.place(x=0,y=80)
+label1= Label(top, text="Masa bloque A, en kilogramos:")
+label1.place(x=0,y=100)
+txt = Entry(top,width=10)
+txt.place(x=180,y=80)
+txt1 = Entry(top,width=10)
+txt1.place(x=180,y=100)
+B.place(x = 50,y = 250)
+top.mainloop()
